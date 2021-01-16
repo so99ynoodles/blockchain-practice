53038bcd2a2f39e1f3dd635d57fb96a04d69f3db
so99ynoodles <so99ynoodles@gmail.com>
2021-01-16 15:23:49 +0900

    Add Lottery.sol

---
+++ lottery/contracts/Lottery.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Lottery {
+    address public manager;
+    address payable[] public players;
+
+    constructor () public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(payable(msg.sender));
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        address payable winner = players[index];
+        winner.transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+}
