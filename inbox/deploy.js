354e89fc3925741f22028729216646ae38d09f1a
so99ynoodles <so99ynoodles@gmail.com>
2021-01-16 13:20:41 +0900

    Add deploy.js

---
+++ inbox/deploy.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider({
+  mnemonic: process.env.MNEMONIC,
+  providerOrUrl: process.env.API_ENDPOINT,
+});
+const web3 = new Web3(provider);
+const ONE_MILLION = "1000000";
+const DEFAULT_MESSAGE = "Hello World";
+const DEFAULT_MESSAGE_BYTES_32 = web3.utils
+  .fromAscii(DEFAULT_MESSAGE)
+  .padEnd(66, "0");
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account ", accounts[0]);
+  const result = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: [DEFAULT_MESSAGE_BYTES_32],
+    })
+    .send({ from: accounts[0], gas: ONE_MILLION });
+
+  console.log("Contract successfully deployed to ", result.options.address);
+};
+
+deploy();
