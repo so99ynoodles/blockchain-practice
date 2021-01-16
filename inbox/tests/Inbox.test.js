8fc646df2c7e636a95e637ecb662381b3f32a4b8
so99ynoodles <so99ynoodles@gmail.com>
2021-01-16 12:53:13 +0900

    Move compile.js

--- inbox/tests/Inbox.test.js
+++ inbox/tests/Inbox.test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const { abi, evm } = require("./compile");
+const { abi, evm } = require("../compile");
 
 const web3 = new Web3(ganache.provider());
 const ONE_MILLION = "1000000";
